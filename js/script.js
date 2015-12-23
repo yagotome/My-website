ae4da13c80e6830516b3c87acaf2c03e3bd42085
Yago Gomes Tomé de Sousa <yagotome_1996@hotmail.com>
2015-12-23 19:28:05 -0200

    My website v 1.0

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
-$(function() {
+$(document).ready(function(){
+$('a[href^="#"]').on('click',function (e) {
+    e.preventDefault();
 
+    var target = this.hash;
+    var $target = $(target);
+
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+    }, 900, 'swing', function () {
+        window.location.hash = target;
+    });
+
+	$('html, body').stop().animate({
+	     'scrollTop': $target.offset().top
+	}, 900, 'swing');
 });
+});
+
+function sendEmail() {
+//verificando se tem algum campo preenchido ainda
+/*$('form-send-email').each(function(){
+	if ($(this).val() === ""){
+		return false;
+	}
+});
+*/
+alert("E-mail successfully sent.\nThank you for the contact!");
+$('#form-send-email').trigger("reset");
+};
